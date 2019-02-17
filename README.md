9d80827327514bf8c6c3ef4cce6ac079d680ed55
Josh <joshdibble93@gmail.com>
2019-02-17 20:29:26 +0000

    HTML & CSS complete. Basic JS begun

--- README.md
+++ README.md
@@ -8,4 +8,4 @@
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
-## Have fun!
+### Have fun!
--- app.js
+++ app.js
@@ -8,3 +8,36 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0"
+document.getElementById("score-1").textContent = "0"
+document.getElementById("current-0").textContent = "0"
+document.getElementById("current-1").textContent = "0"
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    // 1. Random number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display result
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
+
+
+    // 3. Update round score IF the rolled number was NOT a 1
+});
+
+
+
+
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector(".dice").style.display = "none";
